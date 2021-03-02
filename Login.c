37531509d0d7abab1b8851190551ada0012e814e
TbM20 <77915489+TbM20@users.noreply.github.com>
2021-03-02 07:41:40 +0000

    Update Login.c

--- Login.c
+++ Login.c
@@ -1,26 +1,28 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "book_management.h"
-
-Users* userlist;
-int LoginSystem(char* username, char* password) {
-	
-
-	if (strcmp(username, "admin") == 0) {
-		if (strcmp(password, "adminpass") == 0) {
-			return 2;
-		}
-
-	}
-	for (int i = 0; i < userlist[0].totalUsers ;i++) {
-		if (strcmp(userlist[i].username,username)==0) {
-			if (strcmp(userlist[i].password, password) == 0) {
-				return 1;
-			}
-		}
-	}
+#include <stdbool.h>
 
 
-	return 0;
+int LoginSystem(const char *username,const char *password) {
+    int i;
+
+    if (strcmp(username, "admin") == 0) {
+        if (strcmp(password, "adminpass") == 0) {
+            return 61;
+        }
+
+    }
+
+    for (i = 0; i < userlist[0].totalUsers; i++) {
+
+        if (strcmp(userlist[i].username, username) == 0) {
+            if (strcmp(userlist[i].password, password) == 0) {
+                return userlist[i].UserId;
+            }
+        }
+    }
+    return 60;
 }
